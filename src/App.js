d710285d77d9b026ebd18a0abf40f506f2534530
Ilxomjon <ilxomjondeveloper@gmail.com>
2023-01-28 11:40:46 +0500

    Styled Components Intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 // import pages
 import AboutUs from "./pages/AboutUs";
+// import styles
+import GlobalStyle from "./components/GlobalStyle";
 
 const App = () => {
   return (
     <div className="App">
       <h1>e-Learning</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,64 @@
 import React from "react";
 // import images
-import homeIntro from "../images/home_intro.jpg"
+import homeIntro from "../images/home_intro.jpg";
+// import styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>Be educated so that</h2>
-          </div>
-          <div className="hide">
-            <h2>you can change</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
+            <h2>
+              you <span>can change</span>
+            </h2>
+          </Hide>
+          <Hide>
             <h2>the world.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           An educated mind can teach many. An educated mind is better than empty
           one.
         </p>
         <button>About Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={homeIntro} alt="man reading a book" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+// Styled Components
+const About = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 5rem 7rem;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+`;
+
+const Image = styled.div`
+  overflow: hidden;
+  flex: 1;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`
 
 export default AboutSection;
