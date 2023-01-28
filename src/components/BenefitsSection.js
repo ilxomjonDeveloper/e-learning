8564669ce7ac25d1f4acaedc987557d42a2c4305
Ilxomjon <ilxomjondeveloper@gmail.com>
2023-01-28 09:39:16 +0500

    benefit section html completed

---
+++ src/components/BenefitsSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import images
+import homeIntro2 from "../images/homeIntro2.jpg"
+
+const BenefitsSection = () => {
+  return <div className="benefits">
+		<div className="description">
+			<h2>High quality learning system</h2>
+			<div className="benefit">
+				<h3>Online platform</h3>
+				<h3>High experienced mentors</h3>
+				<h3>Fast learning system</h3>
+				<h3>Offline school for children</h3>
+			</div>
+		</div>
+		<img src={homeIntro2} alt="learning process" />
+	</div>;
+};
+
+export default BenefitsSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 //import components
 import AboutSection from "../components/AboutSection";
+import BenefitsSection from "../components/BenefitsSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
+      <BenefitsSection />
     </div>
   );
 };
